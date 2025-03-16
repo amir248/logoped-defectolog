a4af490479a69de3a3cfd4d3c81c517f94234fb1
chikchicly <chikchicly@gmail.com>
2025-03-16 14:00:54 +0700

    menu oK

--- public/site/js/menu500.js
+++ public/site/js/menu500.js
@@ -8,7 +8,7 @@ function openMenu(){
     let scriptMenu=document.createElement('div');
     scriptMenu.classList.add('openMenu');
     scriptMenu.setAttribute('id','menuOpen');
-    scriptMenu.innerHTML=`<a href="/">Главная</a><a href="#services">Услуги</a><a href="#sigCommentsBlock">Отзовы</a><a href="#about">О центре</a><a href="#contact">Контакты</a><a href="Public-Offer">Публичная оферта</a><a href="Privacy-Policy">Политика конфиден<wbr>циальности</a>`;
+    scriptMenu.innerHTML=`<a href="https://fonolog.ru/cure-stuttering">Главная</a><a href="https://fonolog.ru/cure-stuttering#services">Услуги</a><a href="https://fonolog.ru/cure-stuttering#sigCommentsBlock">Отзовы</a><a href="https://fonolog.ru/cure-stuttering#about">О центре</a><a href="https://fonolog.ru/cure-stuttering#contact">Контакты</a><a href="https://fonolog.ru/Public-Offer">Публичная оферта</a><a href="https://fonolog.ru/Privacy-Policy">Политика конфиден<wbr>циальности</a>`;
     document.querySelector('body').append(scriptMenu);
     setTimeout(()=>{
         for(let j=0;j<document.querySelectorAll('.openMenu > a').length; j++){
