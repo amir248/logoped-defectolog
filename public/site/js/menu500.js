e05cc503bf5708bcfbe7829dc42da6a008058c0e
chikchicly <chikchicly@gmail.com>
2025-03-16 13:13:26 +0700

    font-size

--- public/site/js/menu500.js
+++ public/site/js/menu500.js
@@ -8,15 +8,23 @@ function openMenu(){
     let scriptMenu=document.createElement('div');
     scriptMenu.classList.add('openMenu');
     scriptMenu.setAttribute('id','menuOpen');
-    scriptMenu.innerHTML=`<a href=".">Главная</a><a href="#services">Услуги</a><a href="#sigCommentsBlock">Отзовы</a><a href="#about">О центре</a><a href="#contact">Контакты</a>`;
+    scriptMenu.innerHTML=`<a href="/">Главная</a><a href="#services">Услуги</a><a href="#sigCommentsBlock">Отзовы</a><a href="#about">О центре</a><a href="#contact">Контакты</a><a href="Public-Offer">Публичная оферта</a><a href=Privacy-Policy">Политика конфиден<wbr>циальности</a>`;
     document.querySelector('body').append(scriptMenu);
     setTimeout(()=>{
-        document.querySelector('.openMenu').style.width="100%";
+        for(let j=0;j<document.querySelectorAll('.openMenu > a').length; j++){
+            document.querySelectorAll('.openMenu > a')[j].style.fontSize=`20px`;
+        }
+        document.querySelector('.openMenu').style.width=`100%`;
+        document.querySelector('.openMenu').style.opacity=`1`;
+ 
     },100);
     document.getElementById('menuOpen').addEventListener('click',()=>{
         setTimeout(()=>{
         document.querySelector('#menuOpen').remove();
     },400);
         document.querySelector('.openMenu').style.width="0";
+        for(let j=0;j<document.querySelectorAll('.openMenu > a').length; j++){
+            document.querySelectorAll('.openMenu > a')[j].style.fontSize=`0`;
+        }
     });
 };//openMenu
