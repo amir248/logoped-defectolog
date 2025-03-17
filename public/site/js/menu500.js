fa4d5a5d46831dd8e7acf01277dc44bfc898d725
chikchicly <chikchicly@gmail.com>
2025-03-17 08:33:00 +0700

    news info

--- public/site/js/menu500.js
+++ public/site/js/menu500.js
@@ -1,30 +1,28 @@
 if(window.innerWidth<=500){
     document.querySelector("nav").addEventListener('click',()=>{
         openMenu();
-
     });
 }
 function openMenu(){
     let scriptMenu=document.createElement('div');
     scriptMenu.classList.add('openMenu');
     scriptMenu.setAttribute('id','menuOpen');
-    scriptMenu.innerHTML=`<a href="https://fonolog.ru/cure-stuttering">Главная</a><a href="https://fonolog.ru/cure-stuttering#services">Услуги</a><a href="https://fonolog.ru/cure-stuttering#sigCommentsBlock">Отзовы</a><a href="https://fonolog.ru/cure-stuttering#about">О центре</a><a href="https://fonolog.ru/cure-stuttering#contact">Контакты</a><a href="https://fonolog.ru/Public-Offer">Публичная оферта</a><a href="https://fonolog.ru/Privacy-Policy">Политика конфиден<wbr>циальности</a>`;
+    scriptMenu.innerHTML=`<div><a href="https://fonolog.ru/cure-stuttering">Главная</a><a href="https://fonolog.ru/cure-stuttering#services">Услуги</a><a href="https://fonolog.ru/cure-stuttering#sigCommentsBlock">Отзовы</a><a href="https://fonolog.ru/cure-stuttering#about">О центре</a><a href="https://fonolog.ru/cure-stuttering#contact">Контакты</a><a href="https://fonolog.ru/Public-Offer">Публичная оферта</a><a href="https://fonolog.ru/Privacy-Policy">Политика конфиден<wbr>циальности</a></div>`;
     document.querySelector('body').append(scriptMenu);
     setTimeout(()=>{
-        for(let j=0;j<document.querySelectorAll('.openMenu > a').length; j++){
-            document.querySelectorAll('.openMenu > a')[j].style.fontSize=`20px`;
+        for(let j=0;j<document.querySelectorAll('.openMenu > div > a').length; j++){
+            document.querySelectorAll('.openMenu > div > a')[j].style.fontSize=`30px`;
         }
         document.querySelector('.openMenu').style.width=`100%`;
         document.querySelector('.openMenu').style.opacity=`1`;
- 
     },100);
     document.getElementById('menuOpen').addEventListener('click',()=>{
         setTimeout(()=>{
         document.querySelector('#menuOpen').remove();
     },400);
         document.querySelector('.openMenu').style.width="0";
-        for(let j=0;j<document.querySelectorAll('.openMenu > a').length; j++){
-            document.querySelectorAll('.openMenu > a')[j].style.fontSize=`0`;
+        for(let j=0;j<document.querySelectorAll('.openMenu > div > a').length; j++){
+            document.querySelectorAll('.openMenu > div > a')[j].style.fontSize=`0`;
         }
     });
 };//openMenu
