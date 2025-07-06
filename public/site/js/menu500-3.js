d071e516429a64b1dbcb745b185bb1f63ff5b17d
chikchicly <chikchicly@gmail.com>
2025-07-06 20:22:41 +0700

    ok

--- public/site/js/menu500-3.js
+++ public/site/js/menu500-3.js
@@ -7,7 +7,7 @@ function openMenu(){
     let scriptMenu=document.createElement('div');
     scriptMenu.classList.add('openMenu');
     scriptMenu.setAttribute('id','menuOpen');
-    scriptMenu.innerHTML=`<div><a href="#sigCommentsBlock">Отзовы</a><a href="https://fonolog.ru/Public-Offer">Публичная оферта</a><a href="https://fonolog.ru/Privacy-Policy">Политика конфиден<wbr>циальности</a></div>`;
+    scriptMenu.innerHTML=`<div><a href="tel:+79893619813">8-9893619813</a><a href="https://wa.clck.bar/+79893619813">WhatsApp</a><a href="#sigCommentsBlock">Отзовы</a><a href="https://fonolog.ru/Public-Offer">Публичная оферта</a><a href="https://fonolog.ru/Privacy-Policy">Политика конфиден<wbr>циальности</a></div>`;
     document.querySelector('body').append(scriptMenu);
     setTimeout(()=>{
         for(let j=0;j<document.querySelectorAll('.openMenu > div > a').length; j++){
